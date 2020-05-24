31e4a0cfaab66129dcdba3ec4b8f495366a035d9
theheroproject <thenaheroproject@gmail.com>
2020-05-24 16:13:37 +0900

    20200524-1

--- study_ansible/add_ssh_auth.sh
+++ study_ansible/add_ssh_auth.sh
@@ -3,3 +3,5 @@
 #ssh key 생성
 sshpass -p vagrant ssh -T -o STrictHostKeyChecking=no vagrant@node101
 sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@node102
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@node103
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@node104
